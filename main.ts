3525f968f81630a26e523f2b22d76a2ead6c34d0
DockingBay-94 <66800729+DockingBay-94@users.noreply.github.com>
2022-06-20 14:34:48 -0700

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 tiles.setCurrentTilemap(tilemap`level1`)
 let mySprite = sprites.create(assets.image`dah Wizard`, SpriteKind.Player)
-mySprite.setPosition(30, 33)
+mySprite.setPosition(30, 30)
 scene.setBackgroundImage(assets.image`sus sky`)
 scene.cameraFollowSprite(mySprite)
 forever(function () {
-    mySprite.setVelocity(0, 50)
+    mySprite.setVelocity(0, 100)
     if (controller.right.isPressed()) {
         mySprite.x += 2
     }
@@ -12,17 +12,18 @@ forever(function () {
         mySprite.x += -2
     }
     if (controller.up.isPressed()) {
-        mySprite.y += -2
+        mySprite.y += -4
     }
     if (controller.A.isPressed()) {
         mySprite.startEffect(effects.warmRadial)
+        mySprite.setImage(assets.image`dah Wizard but red`)
     } else {
         effects.clearParticles(mySprite)
-    }
-    if (controller.down.isPressed()) {
-    	
+        mySprite.setImage(assets.image`dah Wizard`)
     }
     if (controller.B.isPressed()) {
-        game.reset()
+    	
+    } else {
+    	
     }
 })
