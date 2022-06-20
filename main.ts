433c1df0c87fa0252b989725ba2853ab6759b1f4
DockingBay-94 <66800729+DockingBay-94@users.noreply.github.com>
2022-06-20 13:38:15 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,18 @@
 let mySprite = sprites.create(assets.image`dah Wizard`, SpriteKind.Player)
 forever(function () {
     if (controller.right.isPressed()) {
+        if (controller.right.isPressed()) {
+            mySprite.x += 2
+        }
+        if (controller.left.isPressed()) {
+            mySprite.x += -2
+        }
         mySprite.x += 2
     }
     if (controller.left.isPressed()) {
         mySprite.x += -2
     }
+})
+forever(function () {
+	
 })
