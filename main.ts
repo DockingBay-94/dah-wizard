6b2d79687a84f32c8be430c7af0e8e9f06aa3503
DockingBay-94 <66800729+DockingBay-94@users.noreply.github.com>
2022-06-20 14:51:06 -0700

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,29 +1,35 @@
+scene.setBackgroundImage(assets.image`sus sky`)
 tiles.setCurrentTilemap(tilemap`level1`)
 let mySprite = sprites.create(assets.image`dah Wizard`, SpriteKind.Player)
 mySprite.setPosition(30, 30)
-scene.setBackgroundImage(assets.image`sus sky`)
 scene.cameraFollowSprite(mySprite)
 forever(function () {
-    mySprite.setVelocity(0, 100)
-    if (controller.right.isPressed()) {
-        mySprite.x += 2
-    }
-    if (controller.left.isPressed()) {
-        mySprite.x += -2
-    }
     if (controller.up.isPressed()) {
         mySprite.y += -4
     }
-    if (controller.A.isPressed()) {
-        mySprite.startEffect(effects.warmRadial)
-        mySprite.setImage(assets.image`dah Wizard but red`)
-    } else {
-        effects.clearParticles(mySprite)
+    mySprite.setVelocity(0, 100)
+})
+forever(function () {
+    if (controller.right.isPressed()) {
+        mySprite.x += 2
         mySprite.setImage(assets.image`dah Wizard`)
+        if (controller.A.isPressed()) {
+            mySprite.startEffect(effects.warmRadial)
+            mySprite.setImage(assets.image`dah Wizard but red0`)
+        } else {
+            effects.clearParticles(mySprite)
+        }
     }
-    if (controller.B.isPressed()) {
-    	
-    } else {
-    	
+})
+forever(function () {
+    if (controller.left.isPressed()) {
+        mySprite.x += -2
+        mySprite.setImage(assets.image`dah Wizard but left`)
+        if (controller.A.isPressed()) {
+            mySprite.startEffect(effects.warmRadial)
+            mySprite.setImage(assets.image`dah Wizard but red but left`)
+        } else {
+            effects.clearParticles(mySprite)
+        }
     }
 })
