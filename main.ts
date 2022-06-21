9bd83467f9b2b1570eed5f3b20057373e0cff25a
DockingBay-94 <66800729+DockingBay-94@users.noreply.github.com>
2022-06-21 15:14:26 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,9 @@ tiles.setCurrentTilemap(tilemap`level1`)
 let dah_Wizard = sprites.create(assets.image`dah Wizard right`, SpriteKind.Player)
 tiles.placeOnTile(dah_Wizard, tiles.getTileLocation(2, 14))
 scene.cameraFollowSprite(dah_Wizard)
-let dah_score = textsprite.create("0", 1, 15)
+let dah_score = 0
+let dah_score_bord = textsprite.create("0", 15, 1)
+dah_score_bord.setText(convertToText(dah_score))
 forever(function () {
     if (controller.up.isPressed()) {
         if (dah_Wizard.tileKindAt(TileDirection.Bottom, sprites.castle.tilePath2) || (dah_Wizard.tileKindAt(TileDirection.Bottom, sprites.castle.tilePath1) || dah_Wizard.tileKindAt(TileDirection.Bottom, sprites.castle.tilePath3))) {
@@ -15,6 +17,11 @@ forever(function () {
     }
 })
 forever(function () {
+    if (controller.down.isPressed()) {
+        dah_score += 26
+    }
+})
+forever(function () {
     if (controller.left.isPressed()) {
         dah_Wizard.x += -2
         dah_Wizard.setImage(assets.image`dah Wizard left`)
@@ -23,16 +30,6 @@ forever(function () {
         dah_Wizard.x += 2
         dah_Wizard.setImage(assets.image`dah Wizard right`)
     }
-})
-forever(function () {
-    if (controller.B.isPressed()) {
-        dah_Wizard.setScale(2, ScaleAnchor.Bottom)
-    } else {
-        dah_Wizard.setScale(1, ScaleAnchor.Bottom)
-    }
-})
-forever(function () {
-    dah_score.setPosition(scene.cameraProperty(CameraProperty.X), scene.cameraProperty(CameraProperty.Y) - 53)
 })
 forever(function () {
     if (controller.A.isPressed()) {
@@ -54,7 +51,12 @@ forever(function () {
     }
 })
 forever(function () {
-    if (controller.down.isPressed()) {
-        dah_score.setText("100")
+    dah_score_bord.setPosition(scene.cameraProperty(CameraProperty.X), scene.cameraProperty(CameraProperty.Y) - 53)
+})
+forever(function () {
+    if (controller.B.isPressed()) {
+        dah_Wizard.setScale(2, ScaleAnchor.Bottom)
+    } else {
+        dah_Wizard.setScale(1, ScaleAnchor.Bottom)
     }
 })
