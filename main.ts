0bc1eb2f5b894e04eddaccba96146d1d0cc5903d
DockingBay-94 <66800729+DockingBay-94@users.noreply.github.com>
2022-06-21 16:10:33 -0700

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,15 @@
+let fire_blast: Sprite = null
 scene.setBackgroundImage(assets.image`sus sky`)
 tiles.setCurrentTilemap(tilemap`level1`)
 let dah_Wizard = sprites.create(assets.image`dah Wizard right`, SpriteKind.Player)
 tiles.placeOnTile(dah_Wizard, tiles.getTileLocation(2, 14))
 let bad_guy = sprites.create(assets.image`bad guy`, SpriteKind.Enemy)
-let bad_guy_2 = sprites.create(assets.image`bad guy`, SpriteKind.Enemy)
 tiles.placeOnTile(bad_guy, tiles.getTileLocation(8, 14))
+let bad_guy_2 = sprites.create(assets.image`bad guy`, SpriteKind.Enemy)
+tiles.placeOnTile(bad_guy_2, tiles.getTileLocation(8, 15))
 scene.cameraFollowSprite(dah_Wizard)
 let dah_score = 0
 let dah_score_bord = textsprite.create("0", 15, 1)
-forever(function () {
-    dah_score_bord.setPosition(scene.cameraProperty(CameraProperty.X), scene.cameraProperty(CameraProperty.Y) - 53)
-    dah_score_bord.setText(convertToText(dah_score))
-    if (controller.down.isPressed()) {
-        dah_score += 7
-    }
-})
 forever(function () {
     if (controller.up.isPressed()) {
         if (dah_Wizard.tileKindAt(TileDirection.Bottom, sprites.castle.tilePath2) || (dah_Wizard.tileKindAt(TileDirection.Bottom, sprites.castle.tilePath1) || dah_Wizard.tileKindAt(TileDirection.Bottom, sprites.castle.tilePath3))) {
@@ -26,8 +21,19 @@ forever(function () {
     }
 })
 forever(function () {
+    dah_score_bord.setPosition(scene.cameraProperty(CameraProperty.X), scene.cameraProperty(CameraProperty.Y) - 53)
+    dah_score_bord.setText(convertToText(dah_score))
+    if (controller.down.isPressed()) {
+        dah_score += 7
+    }
+})
+forever(function () {
     bad_guy.follow(dah_Wizard, 10)
     if (dah_Wizard.overlapsWith(bad_guy)) {
+        game.reset()
+    }
+    bad_guy_2.follow(dah_Wizard, 10)
+    if (dah_Wizard.overlapsWith(bad_guy_2)) {
         game.reset()
     }
 })
@@ -43,8 +49,8 @@ forever(function () {
 })
 forever(function () {
     if (controller.A.isPressed()) {
-        dah_Wizard.startEffect(effects.warmRadial)
         if (dah_Wizard.image.equals(assets.image`dah Wizard right`)) {
+            fire_blast = sprites.createProjectileFromSprite(assets.image`fire blast right`, dah_Wizard, 100, 0)
             dah_Wizard.setImage(assets.image`dah Wizard red right`)
         }
         if (dah_Wizard.image.equals(assets.image`dah Wizard left`)) {
@@ -53,6 +59,7 @@ forever(function () {
     } else {
         effects.clearParticles(dah_Wizard)
         if (dah_Wizard.image.equals(assets.image`dah Wizard red left`)) {
+            fire_blast = sprites.createProjectileFromSprite(assets.image`fire blast left`, dah_Wizard, -100, 0)
             dah_Wizard.setImage(assets.image`dah Wizard left`)
         }
         if (dah_Wizard.image.equals(assets.image`dah Wizard red right`)) {
