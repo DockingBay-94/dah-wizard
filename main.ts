64eab99426062cda7aa0dadb020287f58fdac6f8
DockingBay-94 <66800729+DockingBay-94@users.noreply.github.com>
2022-06-20 17:26:00 -0700

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,26 @@
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     dah_Wizard.setScale(0.5, ScaleAnchor.Bottom)
 })
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (dah_Wizard.image.equals(assets.image`dah Wizard right`) || dah_Wizard.image.equals(assets.image`dah Wizard red right`)) {
+        dah_Wizard.startEffect(effects.warmRadial)
+        dah_Wizard.setImage(assets.image`dah Wizard red right`)
+    }
+    if (dah_Wizard.image.equals(assets.image`dah Wizard left`) || dah_Wizard.image.equals(assets.image`dah Wizard red left`)) {
+        dah_Wizard.startEffect(effects.warmRadial)
+        dah_Wizard.setImage(assets.image`dah Wizard red left`)
+    }
+})
+controller.A.onEvent(ControllerButtonEvent.Released, function () {
+    if (dah_Wizard.image.equals(assets.image`dah Wizard red right`)) {
+        effects.clearParticles(dah_Wizard)
+        dah_Wizard.setImage(assets.image`dah Wizard right`)
+    }
+    if (dah_Wizard.image.equals(assets.image`dah Wizard red left`)) {
+        effects.clearParticles(dah_Wizard)
+        dah_Wizard.setImage(assets.image`dah Wizard left`)
+    }
+})
 controller.B.onEvent(ControllerButtonEvent.Released, function () {
     dah_Wizard.setScale(1, ScaleAnchor.Bottom)
 })
@@ -11,14 +31,14 @@ dah_Wizard = sprites.create(assets.image`dah Wizard right`, SpriteKind.Player)
 tiles.placeOnTile(dah_Wizard, tiles.getTileLocation(2, 14))
 scene.cameraFollowSprite(dah_Wizard)
 forever(function () {
-    if (controller.up.isPressed()) {
-        if (dah_Wizard.tileKindAt(TileDirection.Bottom, sprites.castle.tilePath2) || (dah_Wizard.tileKindAt(TileDirection.Bottom, sprites.castle.tilePath1) || dah_Wizard.tileKindAt(TileDirection.Bottom, sprites.castle.tilePath3))) {
-            dah_Wizard.setVelocity(0, -27000)
-        }
-    }
+    dah_Wizard.setVelocity(0, 50)
 })
 forever(function () {
-    dah_Wizard.setVelocity(0, 50)
+    if (controller.up.isPressed()) {
+        if (dah_Wizard.tileKindAt(TileDirection.Bottom, sprites.castle.tilePath2) || (dah_Wizard.tileKindAt(TileDirection.Bottom, sprites.castle.tilePath1) || dah_Wizard.tileKindAt(TileDirection.Bottom, sprites.castle.tilePath3))) {
+            dah_Wizard.setVelocity(0, -200)
+        }
+    }
 })
 forever(function () {
     if (controller.left.isPressed()) {
@@ -31,14 +51,5 @@ forever(function () {
     }
 })
 forever(function () {
-    if (controller.A.isPressed()) {
-        dah_Wizard.startEffect(effects.warmRadial)
-        if (dah_Wizard.image.equals(assets.image`dah Wizard right`)) {
-            dah_Wizard.setImage(assets.image`dah Wizard red right`)
-        } else if (dah_Wizard.image.equals(assets.image`dah Wizard left`)) {
-            dah_Wizard.setImage(assets.image`dah Wizard red left`)
-        }
-    } else {
-        effects.clearParticles(dah_Wizard)
-    }
+	
 })
