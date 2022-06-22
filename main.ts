0858d8c9a5a10935312bf748dbc7ba6d1ad41bae
DockingBay-94 <66800729+DockingBay-94@users.noreply.github.com>
2022-06-22 14:45:22 -0700

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,27 @@
+function setLevel () {
+    scene.setBackgroundImage(assets.image`sus sky`)
+    tiles.setCurrentTilemap(tilemap`level1`)
+    da_score_bord = textsprite.create("0", 15, 1)
+}
 function placeBadGuy (x: number, y: number) {
     bad_guy = sprites.create(assets.image`bad guy`, SpriteKind.Enemy)
-    bad_guy.follow(dah_Wizard, 10)
+    bad_guy.follow(da_Wizard, 10)
     tiles.placeOnTile(bad_guy, tiles.getTileLocation(x, y))
 }
+function setPlayer () {
+    da_Wizard = sprites.create(assets.image`dah Wizard right`, SpriteKind.Player)
+    tiles.placeOnTile(da_Wizard, tiles.getTileLocation(2, 14))
+    scene.cameraFollowSprite(da_Wizard)
+}
 function fireBlastDirection (initImg: Image, targetImg: Image, fireImg: Image, fireVel: number) {
-    if (dah_Wizard.image.equals(initImg)) {
-        fire_blast = sprites.createProjectileFromSprite(fireImg, dah_Wizard, fireVel, 0)
-        dah_Wizard.setImage(targetImg)
+    if (da_Wizard.image.equals(initImg)) {
+        fire_blast = sprites.createProjectileFromSprite(fireImg, da_Wizard, fireVel, 0)
+        da_Wizard.setImage(targetImg)
     }
+}
+function movePlayer1 (num: number, myImage: Image) {
+    da_Wizard.x += num
+    da_Wizard.setImage(myImage)
 }
 function fireBlast () {
     fireBlastDirection(assets.image`dah Wizard right`, assets.image`dah Wizard red right`, assets.image`fire blast right`, 100)
@@ -18,13 +32,9 @@ function skinRevertToBlue () {
     skinRevertAndDirection(assets.image`dah Wizard red left`, assets.image`dah Wizard left`)
 }
 function skinRevertAndDirection (initImg: Image, targetImg: Image) {
-    if (dah_Wizard.image.equals(initImg)) {
-        dah_Wizard.setImage(targetImg)
+    if (da_Wizard.image.equals(initImg)) {
+        da_Wizard.setImage(targetImg)
     }
-}
-function move (num: number, myImage: Image) {
-    dah_Wizard.x += num
-    dah_Wizard.setImage(myImage)
 }
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
     otherSprite.destroy()
@@ -36,43 +46,39 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSp
 })
 let dah_score = 0
 let fire_blast: Sprite = null
+let da_Wizard: Sprite = null
 let bad_guy: Sprite = null
-let dah_Wizard: Sprite = null
-scene.setBackgroundImage(assets.image`sus sky`)
-tiles.setCurrentTilemap(tilemap`level1`)
-dah_Wizard = sprites.create(assets.image`dah Wizard right`, SpriteKind.Player)
-tiles.placeOnTile(dah_Wizard, tiles.getTileLocation(2, 14))
-scene.cameraFollowSprite(dah_Wizard)
-let dah_score_bord = textsprite.create("0", 15, 1)
+let da_score_bord: TextSprite = null
+setLevel()
+setPlayer()
 placeBadGuy(8, 14)
 placeBadGuy(8, 15)
 forever(function () {
-    if (controller.up.isPressed()) {
-        if (dah_Wizard.tileKindAt(TileDirection.Bottom, sprites.castle.tilePath2) || (dah_Wizard.tileKindAt(TileDirection.Bottom, sprites.castle.tilePath1) || dah_Wizard.tileKindAt(TileDirection.Bottom, sprites.castle.tilePath3))) {
-            dah_Wizard.setVelocity(0, -100)
-            pause(200)
-        }
+    if (controller.up.isPressed() && (da_Wizard.tileKindAt(TileDirection.Bottom, sprites.castle.tilePath2) || (da_Wizard.tileKindAt(TileDirection.Bottom, sprites.castle.tilePath1) || da_Wizard.tileKindAt(TileDirection.Bottom, sprites.castle.tilePath3)))) {
+        pause(100)
+        da_Wizard.setVelocity(0, -100)
+        pause(200)
     } else {
-        dah_Wizard.setVelocity(0, 100)
+        da_Wizard.setVelocity(0, 100)
     }
 })
 forever(function () {
     if (controller.right.isPressed()) {
-        move(2, assets.image`dah Wizard right`)
+        movePlayer1(2, assets.image`dah Wizard right`)
     }
     if (controller.left.isPressed()) {
-        move(-2, assets.image`dah Wizard left`)
+        movePlayer1(-2, assets.image`dah Wizard left`)
     }
 })
 forever(function () {
-    dah_score_bord.setPosition(scene.cameraProperty(CameraProperty.X), scene.cameraProperty(CameraProperty.Y) - 53)
-    dah_score_bord.setText(convertToText(dah_score))
+    da_score_bord.setPosition(scene.cameraProperty(CameraProperty.X), scene.cameraProperty(CameraProperty.Y) - 53)
+    da_score_bord.setText(convertToText(dah_score))
 })
 forever(function () {
     if (controller.B.isPressed()) {
-        dah_Wizard.setScale(2, ScaleAnchor.Bottom)
+        da_Wizard.setScale(2, ScaleAnchor.Bottom)
     } else {
-        dah_Wizard.setScale(1, ScaleAnchor.Bottom)
+        da_Wizard.setScale(1, ScaleAnchor.Bottom)
     }
 })
 forever(function () {
