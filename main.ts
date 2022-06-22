515056c5bbb3613a827efca7fa1241d6347b7fda
DockingBay-94 <66800729+DockingBay-94@users.noreply.github.com>
2022-06-21 17:04:44 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,9 @@
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    otherSprite.destroy()
+    sprite.destroy()
+})
 let fire_blast: Sprite = null
+let dah_score = 0
 scene.setBackgroundImage(assets.image`sus sky`)
 tiles.setCurrentTilemap(tilemap`level1`)
 let dah_Wizard = sprites.create(assets.image`dah Wizard right`, SpriteKind.Player)
@@ -8,7 +13,6 @@ tiles.placeOnTile(bad_guy, tiles.getTileLocation(8, 14))
 let bad_guy_2 = sprites.create(assets.image`bad guy`, SpriteKind.Enemy)
 tiles.placeOnTile(bad_guy_2, tiles.getTileLocation(8, 15))
 scene.cameraFollowSprite(dah_Wizard)
-let dah_score = 0
 let dah_score_bord = textsprite.create("0", 15, 1)
 forever(function () {
     if (controller.up.isPressed()) {
@@ -18,23 +22,6 @@ forever(function () {
         }
     } else {
         dah_Wizard.setVelocity(0, 100)
-    }
-})
-forever(function () {
-    dah_score_bord.setPosition(scene.cameraProperty(CameraProperty.X), scene.cameraProperty(CameraProperty.Y) - 53)
-    dah_score_bord.setText(convertToText(dah_score))
-    if (controller.down.isPressed()) {
-        dah_score += 7
-    }
-})
-forever(function () {
-    bad_guy.follow(dah_Wizard, 10)
-    if (dah_Wizard.overlapsWith(bad_guy)) {
-        game.reset()
-    }
-    bad_guy_2.follow(dah_Wizard, 10)
-    if (dah_Wizard.overlapsWith(bad_guy_2)) {
-        game.reset()
     }
 })
 forever(function () {
@@ -48,18 +35,31 @@ forever(function () {
     }
 })
 forever(function () {
+    dah_score_bord.setPosition(scene.cameraProperty(CameraProperty.X), scene.cameraProperty(CameraProperty.Y) - 53)
+    dah_score_bord.setText(convertToText(dah_score))
+})
+forever(function () {
+    if (controller.B.isPressed()) {
+        dah_Wizard.setScale(2, ScaleAnchor.Bottom)
+    } else {
+        dah_Wizard.setScale(1, ScaleAnchor.Bottom)
+    }
+    if (controller.down.isPressed()) {
+        dah_score += 7
+    }
+})
+forever(function () {
     if (controller.A.isPressed()) {
         if (dah_Wizard.image.equals(assets.image`dah Wizard right`)) {
             fire_blast = sprites.createProjectileFromSprite(assets.image`fire blast right`, dah_Wizard, 100, 0)
             dah_Wizard.setImage(assets.image`dah Wizard red right`)
         }
         if (dah_Wizard.image.equals(assets.image`dah Wizard left`)) {
+            fire_blast = sprites.createProjectileFromSprite(assets.image`fire blast left`, dah_Wizard, -100, 0)
             dah_Wizard.setImage(assets.image`dah Wizard red left`)
         }
     } else {
-        effects.clearParticles(dah_Wizard)
         if (dah_Wizard.image.equals(assets.image`dah Wizard red left`)) {
-            fire_blast = sprites.createProjectileFromSprite(assets.image`fire blast left`, dah_Wizard, -100, 0)
             dah_Wizard.setImage(assets.image`dah Wizard left`)
         }
         if (dah_Wizard.image.equals(assets.image`dah Wizard red right`)) {
@@ -68,9 +68,20 @@ forever(function () {
     }
 })
 forever(function () {
-    if (controller.B.isPressed()) {
-        dah_Wizard.setScale(2, ScaleAnchor.Bottom)
-    } else {
-        dah_Wizard.setScale(1, ScaleAnchor.Bottom)
+    bad_guy.follow(dah_Wizard, 10)
+    if (dah_Wizard.overlapsWith(bad_guy)) {
+        game.reset()
+    }
+    if (fire_blast && fire_blast.overlapsWith(bad_guy)) {
+        bad_guy.destroy()
+    }
+})
+forever(function () {
+    bad_guy_2.follow(dah_Wizard, 10)
+    if (dah_Wizard.overlapsWith(bad_guy_2)) {
+        game.reset()
+    }
+    if (fire_blast && fire_blast.overlapsWith(bad_guy_2)) {
+        bad_guy_2.destroy()
     }
 })
