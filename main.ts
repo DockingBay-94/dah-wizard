57abfb4f056fe27c17bac0440ab6733429d8b1a9
DockingBay-94 <66800729+DockingBay-94@users.noreply.github.com>
2022-06-22 13:36:26 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,49 @@
+function placeBadGuy (x: number, y: number) {
+    bad_guy = sprites.create(assets.image`bad guy`, SpriteKind.Enemy)
+    tiles.placeOnTile(bad_guy, tiles.getTileLocation(x, y))
+}
+function skinToBlue () {
+    if (dah_Wizard.image.equals(assets.image`dah Wizard red left`)) {
+        dah_Wizard.setImage(assets.image`dah Wizard left`)
+    }
+    if (dah_Wizard.image.equals(assets.image`dah Wizard red right`)) {
+        dah_Wizard.setImage(assets.image`dah Wizard right`)
+    }
+}
+function fireBlast () {
+    if (dah_Wizard.image.equals(assets.image`dah Wizard right`)) {
+        fire_blast = sprites.createProjectileFromSprite(assets.image`fire blast right`, dah_Wizard, 100, 0)
+        dah_Wizard.setImage(assets.image`dah Wizard red right`)
+    }
+    if (dah_Wizard.image.equals(assets.image`dah Wizard left`)) {
+        fire_blast = sprites.createProjectileFromSprite(assets.image`fire blast left`, dah_Wizard, -100, 0)
+        dah_Wizard.setImage(assets.image`dah Wizard red left`)
+    }
+}
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
-    otherSprite.destroy()
-    sprite.destroy()
+	
 })
-let fire_blast: Sprite = null
 let dah_score = 0
+let fire_blast: Sprite = null
+let bad_guy: Sprite = null
+let dah_Wizard: Sprite = null
 scene.setBackgroundImage(assets.image`sus sky`)
 tiles.setCurrentTilemap(tilemap`level1`)
-let dah_Wizard = sprites.create(assets.image`dah Wizard right`, SpriteKind.Player)
+dah_Wizard = sprites.create(assets.image`dah Wizard right`, SpriteKind.Player)
 tiles.placeOnTile(dah_Wizard, tiles.getTileLocation(2, 14))
-let bad_guy = sprites.create(assets.image`bad guy`, SpriteKind.Enemy)
-tiles.placeOnTile(bad_guy, tiles.getTileLocation(8, 14))
-let bad_guy_2 = sprites.create(assets.image`bad guy`, SpriteKind.Enemy)
-tiles.placeOnTile(bad_guy_2, tiles.getTileLocation(8, 15))
 scene.cameraFollowSprite(dah_Wizard)
 let dah_score_bord = textsprite.create("0", 15, 1)
+placeBadGuy(8, 14)
+placeBadGuy(8, 15)
+forever(function () {
+    bad_guy.follow(dah_Wizard, 10)
+    if (dah_Wizard.overlapsWith(bad_guy)) {
+        game.reset()
+    }
+    if (fire_blast && fire_blast.overlapsWith(bad_guy)) {
+        bad_guy.destroy()
+    }
+})
 forever(function () {
     if (controller.up.isPressed()) {
         if (dah_Wizard.tileKindAt(TileDirection.Bottom, sprites.castle.tilePath2) || (dah_Wizard.tileKindAt(TileDirection.Bottom, sprites.castle.tilePath1) || dah_Wizard.tileKindAt(TileDirection.Bottom, sprites.castle.tilePath3))) {
@@ -50,38 +80,8 @@ forever(function () {
 })
 forever(function () {
     if (controller.A.isPressed()) {
-        if (dah_Wizard.image.equals(assets.image`dah Wizard right`)) {
-            fire_blast = sprites.createProjectileFromSprite(assets.image`fire blast right`, dah_Wizard, 100, 0)
-            dah_Wizard.setImage(assets.image`dah Wizard red right`)
-        }
-        if (dah_Wizard.image.equals(assets.image`dah Wizard left`)) {
-            fire_blast = sprites.createProjectileFromSprite(assets.image`fire blast left`, dah_Wizard, -100, 0)
-            dah_Wizard.setImage(assets.image`dah Wizard red left`)
-        }
+        fireBlast()
     } else {
-        if (dah_Wizard.image.equals(assets.image`dah Wizard red left`)) {
-            dah_Wizard.setImage(assets.image`dah Wizard left`)
-        }
-        if (dah_Wizard.image.equals(assets.image`dah Wizard red right`)) {
-            dah_Wizard.setImage(assets.image`dah Wizard right`)
-        }
-    }
-})
-forever(function () {
-    bad_guy.follow(dah_Wizard, 10)
-    if (dah_Wizard.overlapsWith(bad_guy)) {
-        game.reset()
-    }
-    if (fire_blast && fire_blast.overlapsWith(bad_guy)) {
-        bad_guy.destroy()
-    }
-})
-forever(function () {
-    bad_guy_2.follow(dah_Wizard, 10)
-    if (dah_Wizard.overlapsWith(bad_guy_2)) {
-        game.reset()
-    }
-    if (fire_blast && fire_blast.overlapsWith(bad_guy_2)) {
-        bad_guy_2.destroy()
+        skinToBlue()
     }
 })
