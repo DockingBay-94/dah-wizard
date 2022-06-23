c1aff451f1ca48bf20ba7b76639e2c3cd882ac9b
DockingBay-94 <66800729+DockingBay-94@users.noreply.github.com>
2022-06-23 15:55:53 -0700

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -3,34 +3,35 @@ function setLevel () {
     tiles.setCurrentTilemap(tilemap`level1`)
     da_score_bord = textsprite.create("0", 15, 1)
 }
-function skinChange (initImg: Image, targetImg: Image) {
-    if (da_Wizard.image.equals(initImg)) {
-        da_Wizard.setImage(targetImg)
+function skinChange (initImg: Image, targetImg: Image, who: Sprite) {
+    if (who.image.equals(initImg)) {
+        who.setImage(targetImg)
     }
 }
-function jumpAcc () {
-    pause(50)
-    da_Wizard.setVelocity(0, -200)
-    pause(50)
-    da_Wizard.setVelocity(0, -100)
-    pause(50)
-    da_Wizard.setVelocity(0, -50)
-}
 function placeBadGuy (x: number, y: number) {
-    bad_guy = sprites.create(assets.image`bad guy`, SpriteKind.Enemy)
-    bad_guy.follow(da_Wizard, 10)
-    tiles.placeOnTile(bad_guy, tiles.getTileLocation(x, y))
+    bad_guys.push(sprites.create(assets.image`bad guy stage1`, SpriteKind.Enemy))
+    bad_guys[bad_guys.length - 1].follow(da_Wizard, 10)
+    tiles.placeOnTile(bad_guys[bad_guys.length - 1], tiles.getTileLocation(x, y))
 }
 function fireBlastDirection (initImg: Image, targetImg: Image, fireImg: Image, fireVel: number) {
     if (da_Wizard.image.equals(initImg)) {
-        fire_blast = sprites.createProjectileFromSprite(fireImg, da_Wizard, fireVel, 0)
         da_Wizard.setImage(targetImg)
+        pause(200)
+        fire_blasts.push(sprites.createProjectileFromSprite(fireImg, da_Wizard, fireVel, 0))
     }
 }
 function setPlayer1 () {
     da_Wizard = sprites.create(assets.image`Da Wizard right`, SpriteKind.Player)
     tiles.placeOnTile(da_Wizard, tiles.getTileLocation(2, 14))
     scene.cameraFollowSprite(da_Wizard)
+}
+function playerJump () {
+    pause(50)
+    da_Wizard.setVelocity(0, -200)
+    pause(50)
+    da_Wizard.setVelocity(0, -100)
+    pause(50)
+    da_Wizard.setVelocity(0, -50)
 }
 function movePlayer1 (num: number, myImage: Image) {
     da_Wizard.x += num
@@ -41,14 +42,9 @@ function fireBlast () {
     fireBlastDirection(assets.image`Da Wizard left`, assets.image`Da Wizard red left`, assets.image`fire blast left`, -100)
 }
 function skinRevertToBlue () {
-    skinChange(assets.image`Da Wizard red right`, assets.image`Da Wizard right`)
-    skinChange(assets.image`Da Wizard red left`, assets.image`Da Wizard left`)
+    skinChange(assets.image`Da Wizard red right`, assets.image`Da Wizard right`, da_Wizard)
+    skinChange(assets.image`Da Wizard red left`, assets.image`Da Wizard left`, da_Wizard)
 }
-sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
-    otherSprite.destroy()
-    sprite.destroy()
-    dah_score += 7
-})
 function placeAllBadGuys () {
     placeBadGuy(8, 14)
     placeBadGuy(13, 12)
@@ -62,32 +58,53 @@ function placeAllBadGuys () {
     placeBadGuy(32, 13)
     placeBadGuy(33, 13)
     placeBadGuy(34, 13)
-    placeBadGuy(30, 13)
-    placeBadGuy(31, 13)
-    placeBadGuy(32, 13)
-    placeBadGuy(33, 13)
-    placeBadGuy(34, 13)
+    placeBadGuy(35, 12)
+    placeBadGuy(36, 13)
+    placeBadGuy(37, 12)
+    placeBadGuy(38, 13)
+    placeBadGuy(39, 12)
 }
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
-    game.over(false)
-})
-let dah_score = 0
-let fire_blast: Sprite = null
-let bad_guy: Sprite = null
+let da_score = 0
+let fire_blasts: Sprite[] = []
 let da_Wizard: Sprite = null
+let bad_guys: Sprite[] = []
 let da_score_bord: TextSprite = null
 setLevel()
 setPlayer1()
-forever(function () {
-	
-})
+placeAllBadGuys()
 forever(function () {
     da_score_bord.setPosition(scene.cameraProperty(CameraProperty.X), scene.cameraProperty(CameraProperty.Y) - 53)
-    da_score_bord.setText(convertToText(dah_score))
+    da_score_bord.setText(convertToText(da_score))
+})
+forever(function () {
+    for (let currentFireBlast of fire_blasts) {
+        for (let currentBadGuy of bad_guys) {
+            if (currentBadGuy.overlapsWith(currentFireBlast)) {
+                if (assets.image`bad guy stage1` <= currentBadGuy.image) {
+                    currentFireBlast.destroy()
+                    skinChange(assets.image`bad guy stage1`, assets.image`bad guy stage2`, currentBadGuy)
+                    pause(500)
+                }
+            }
+            if (currentBadGuy.overlapsWith(currentFireBlast)) {
+                if (currentBadGuy.image <= assets.image`bad guy stage2`) {
+                    currentBadGuy.destroy()
+                    da_score += 10
+                }
+            }
+        }
+    }
+})
+forever(function () {
+    for (let attackingBadGuy of bad_guys) {
+        if (attackingBadGuy.overlapsWith(da_Wizard)) {
+        	
+        }
+    }
 })
 forever(function () {
     if (controller.up.isPressed() && (da_Wizard.tileKindAt(TileDirection.Bottom, assets.tile`Grass up`) || (da_Wizard.tileKindAt(TileDirection.Bottom, assets.tile`Grass left up`) || da_Wizard.tileKindAt(TileDirection.Bottom, assets.tile`Grass right up`)))) {
-        jumpAcc()
+        playerJump()
     } else {
         da_Wizard.ay = 200
     }
@@ -101,11 +118,6 @@ forever(function () {
         fireBlast()
     } else {
         skinRevertToBlue()
-    }
-    if (controller.B.isPressed()) {
-        da_Wizard.setScale(2, ScaleAnchor.Bottom)
-    } else {
-        da_Wizard.setScale(1, ScaleAnchor.Bottom)
     }
 })
 forever(function () {
