c6ce156b23d83889f98ac5013e3a37c74e798326
DockingBay-94 <66800729+DockingBay-94@users.noreply.github.com>
2022-06-24 14:40:39 -0700

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -8,11 +8,12 @@ function skinChange (initImg: Image, targetImg: Image, who: Sprite) {
         who.setImage(targetImg)
     }
 }
-function placeBadGuy (x: number, y: number) {
+function placeBadGuy (x: number, y: number, size: number) {
     bad_guys_sprites.push(sprites.create(assets.image`bad guy stage1`, SpriteKind.Enemy))
     badGuyState.push("normal")
     bad_guys_sprites[bad_guys_sprites.length - 1].follow(da_Wizard, 10)
     tiles.placeOnTile(bad_guys_sprites[bad_guys_sprites.length - 1], tiles.getTileLocation(x, y))
+    bad_guys_sprites[bad_guys_sprites.length - 1].setScale(size, ScaleAnchor.Middle)
 }
 function fireBlastDirection (initImg: Image, targetImg: Image, fireImg: Image, fireVel: number) {
     if (da_Wizard.image.equals(initImg)) {
@@ -47,23 +48,23 @@ function skinRevertToBlue () {
     skinChange(assets.image`Da Wizard red left`, assets.image`Da Wizard left`, da_Wizard)
 }
 function placeAllBadGuys () {
-    placeBadGuy(8, 14)
-    placeBadGuy(13, 12)
-    placeBadGuy(14, 14)
-    placeBadGuy(17, 11)
-    placeBadGuy(21, 11)
-    placeBadGuy(23, 12)
-    placeBadGuy(27, 15)
-    placeBadGuy(30, 13)
-    placeBadGuy(31, 13)
-    placeBadGuy(32, 13)
-    placeBadGuy(33, 13)
-    placeBadGuy(34, 13)
-    placeBadGuy(35, 13)
-    placeBadGuy(36, 13)
-    placeBadGuy(37, 13)
-    placeBadGuy(38, 13)
-    placeBadGuy(39, 13)
+    placeBadGuy(8, 14, 1)
+    placeBadGuy(13, 12, 1)
+    placeBadGuy(14, 14, 1)
+    placeBadGuy(17, 11, 1)
+    placeBadGuy(21, 11, 1)
+    placeBadGuy(23, 12, 1)
+    placeBadGuy(27, 15, 1)
+    placeBadGuy(30, 13, 1)
+    placeBadGuy(31, 13, 1)
+    placeBadGuy(32, 13, 1)
+    placeBadGuy(33, 13, 1)
+    placeBadGuy(34, 13, 1)
+    placeBadGuy(35, 13, 1)
+    placeBadGuy(36, 13, 1)
+    placeBadGuy(37, 13, 1)
+    placeBadGuy(38, 13, 1)
+    placeBadGuy(39, 13, 1)
 }
 let da_score = 0
 let da_Wizard: Sprite = null
@@ -128,7 +129,12 @@ forever(function () {
 })
 forever(function () {
     if (da_Wizard.tileKindAt(TileDirection.Top, sprites.dungeon.collectibleInsignia)) {
-        tiles.placeOnTile(da_Wizard, tiles.getTileLocation(55, 14))
+        tiles.placeOnTile(da_Wizard, tiles.getTileLocation(55, 18))
         scene.setBackgroundImage(assets.image`castle`)
+        placeBadGuy(65, 16, 2)
+        placeBadGuy(71, 17, 2)
+        placeBadGuy(77, 16, 2)
+        placeBadGuy(81, 17, 2)
+        placeBadGuy(84, 16, 2)
     }
 })
